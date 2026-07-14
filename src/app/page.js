1c3f7cd830a80dbfecb4146a02295e377a925d6f
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-07-15 00:16:22 +0600

    learn:HeroUi

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
+import { Button } from "@heroui/react";
+
 export default function Home() {
   return (   
-    <div>
-       
+    <div className="mx-auto">
+       <Button variant="tertiary">Hero Ui</Button>
     </div>    
   );
 }
